110f6013e25b3f8cffcd0eddea5d9c906467d33b
Pedram <posquian@gmail.com>
2025-07-04 16:44:50 +0330

    Login/Signup JSON (Needs JSON adjustments)

--- src/main/java/com/example/unitransit/Datamanagement.java
+++ src/main/java/com/example/unitransit/Datamanagement.java
@@ -1,6 +1,7 @@
 package com.example.unitransit;
 
 import com.example.unitransit.model.Student;
+import com.example.unitransit.model.University;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -15,6 +16,7 @@ import java.util.List;
 
 public class Datamanagement {
     private static final String USERS_FILE = "users.json";
+    private static final String UNI_FILE = "universities.json";
 
     private final Gson gson;
     public Datamanagement() {
@@ -36,6 +38,24 @@ public class Datamanagement {
             System.out.println("Saving users to " + USERS_FILE);
         } catch (IOException e) {
             System.out.println("Error saving users to " + USERS_FILE);
+        }
+    }
+
+    public List<University> loadUniversity() {
+        try (FileReader reader = new FileReader(UNI_FILE)) {
+            Type universityListType=new TypeToken<List<Student>>() {}.getType();
+            return gson.fromJson(reader, universityListType);
+        } catch (IOException e) {
+            System.out.println("Error loading universities file");
+            return new ArrayList<>();
+        }
+    }
+    public void saveUniversity(List<University> university) {
+        try (FileWriter writer = new FileWriter(UNI_FILE)) {
+            gson.toJson(university, writer);
+            System.out.println("Saving users to " + UNI_FILE);
+        } catch (IOException e) {
+            System.out.println("Error saving users to " + UNI_FILE);
         }
     }
 }
---
+++ src/main/java/com/example/unitransit/model/University.java
@@ -0,0 +1,11 @@
+package com.example.unitransit.model;
+
+public class University {
+    String name;
+    public University(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
